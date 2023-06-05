bee32eafa29a13f98fdf33b868dc357313e8c45a
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-06-05 12:49:08 +0200

    Add files via upload

---
+++ src/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   minishell.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/02 09:45:46 by jmetzger      #+#    #+#                 */
+/*   Updated: 2023/06/05 12:32:58 by jmetzger      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+#include <readline/readline.h>
+ #include <readline/history.h>
+
+int	ft_strcmp(char *s1, char *s2);
+
+#endif
