7115a6505b76d4c89e7eb8bf339639afe9c66c85
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-06-07 14:45:10 +0200

    Update ft_echo.c

--- src/ft_echo.c
+++ src/ft_echo.c
@@ -12,7 +12,38 @@
 
 #include "minishell.h"
 
-void ft_echo()
+int static ft_argc(char **input)
 {
+    int len;
     
+    len = 0;
+    while (input[len])
+        len++;
+    return (len);
+}
+
+void ft_echo(char **input)
+{
+    int i;
+    int n;
+
+    i = 1;
+    n = 0;
+    if (ft_argc(input) > 1)
+    {
+        while (input[i] && ft_strcmp(input[i], "-n") == 0)
+        {
+            n = 1;
+            i++;
+        }
+        while (input[i])
+        {
+            printf("%s ", input[i]);
+            if (input[i + 1] && input[i][0] != '\0')
+                printf(" ");
+            i++;
+        }
+        if (n == 0)
+            printf("\n");
+    }
 }
