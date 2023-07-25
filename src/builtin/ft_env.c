fbed401f2abdf5df053631af941274861eae9780
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-07-25 22:06:04 +0200

    Update ft_env.c

--- src/builtin/ft_env.c
+++ src/builtin/ft_env.c
@@ -6,22 +6,32 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/06 16:37:54 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/07/11 10:42:57 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/07/25 15:12:49 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int ft_env(t_data *data)
+/* ft_env();
+ *	- t_data *data: main struct, access the environment variables;
+ *
+ *	- This function prints the environment variables that are marked 
+ *	  with the 'for_export' state.
+ *	- It loops through the linked list of environment variables 'data->env'.
+ *	- If an environment variable has the 'for_export' state set to true, 
+ *	  it will be printed in the format "name=value".
+ *	- Returns EXIT_SUCCESS to indicate successful execution.
+ */
+int	ft_env(t_data *data)
 {
-   t_env *env;
+	t_env	*env;
 
-   env = data->env;
-   while (env != NULL)
-   {
-      if (env->for_export)
-         printf("%s=%s\n", env->name, env->value);
-      env = env->next;
-   }
-   return (EXIT_SUCCESS);
+	env = data->env;
+	while (env != NULL)
+	{
+		if (env->for_export)
+			printf("%s=%s\n", env->name, env->value);
+		env = env->next;
+	}
+	return (EXIT_SUCCESS);
 }
