85d3c6747edce3711ae8b2e3cd02a5a4eabdf098
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-07-26 21:26:45 +0200

    Update signals.c

--- src/utils/signals.c
+++ src/utils/signals.c
@@ -6,45 +6,84 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/06 10:48:39 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/07/20 14:29:28 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/07/26 21:25:02 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-long long int g_exit_status;
+/*
+ * I am using a global variable to store the exit status of signals.
+ * But because the 'signal()' function can only return 'void', 
+ * so we use this global variable to pass the exit status.
+ * Since 'signal()' can't directly pass a struct, 
+ * using a global variable is a workaround to capture the exit status.
+ */
+long long int	g_exit_status;
 
+/* restore_prompt();
+ *	- Signal handler for SIGINT (Ctrl-C) in the parent process.
+ *	- Displays a new prompt and updates the exit status.
+ */
 void	restore_prompt(int sig)
 {
 	if (sig == SIGINT)
-    {
-        ft_putchar_fd('\n', STDOUT_FILENO);
+	{
+		ft_putchar_fd('\n', STDOUT_FILENO);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
 		g_exit_status = 128 + sig;
-    }
+	}
 }
 
+/* ctrl_c();
+ *	- Signal handler for SIGINT (Ctrl-C) in a child process.
+ *	- Exits the current child process and updates the exit status.
+ */
 void	ctrl_c(int sig)
 {
 	if (sig == SIGINT)
-    {
-        ft_putchar_fd('\n', STDOUT_FILENO);
-        g_exit_status = 128 + sig;
-    }
+	{
+		ft_putchar_fd('\n', STDOUT_FILENO);
+		g_exit_status = 128 + sig;
+	}
 }
 
+/* backslash();
+ *	- Signal handler for SIGQUIT (Ctrl-\).
+ *	- Writes "Quit" to the output and updates the exit status.
+ */
 void	backslash(int sig)
 {
-    if (sig == SIGQUIT)
-    {
-	    printf("Quit\n");
-        g_exit_status = 128 + sig;
-    }
+	if (sig == SIGQUIT)
+	{
+		printf("Quit\n");
+		g_exit_status = 128 + sig;
+	}
 }
 
-void	handle_signal(int num)
+/* handle_signal();
+ *	- int num: the signal number to handle 
+ *	  (1 for parent process, 2 for child process);
+ *	- t_data *data: main struct, for the exit status;
+ *
+ *	- Configures the signal handling for the minishell.
+ *	- When 'num' is 1 (signal is in the parent process):
+ *		- SIGINT is set to call the 'restore_prompt' function 
+ *		  when received (used for Ctrl-C).
+ *		- SIGQUIT is set to be ignored (used for Ctrl-\).
+ *	- When 'num' is 2 (signal is in a child process):
+ *		- SIGINT is set to call the 'ctrl_c' function 
+ *		  when received (used for Ctrl-C).
+ *		- SIGQUIT is set to call the 'backslash' function 
+ *		  when received (used for Ctrl-\).
+ *	- It updates the 'data->status' with the value of 'g_exit_status'.
+ *
+ *	NOTE: Ctrl-D is not a signal it is a EOF discripter. 
+ *		  So Ctrl-D gets handeld in the main().
+ */
+void	handle_signal(int num, t_data *data)
 {
 	if (num == 1)
 	{
@@ -56,17 +95,5 @@ void	handle_signal(int num)
 		signal(SIGINT, ctrl_c);
 		signal(SIGQUIT, backslash);
 	}
+	data->status = g_exit_status;
 }
-
-/* The value 130 is calculated by adding 128 to the signal number. 
-*   When a process receives a SIGINT signal, the default action is to terminate the process. 
-*   By convention, the shell uses an exit status of 128 + signal number to indicate 
-*   that the process was terminated due to a signal. In the case of SIGINT (signal number 2), 
-*   adding 128 gives 130.
-*/
-
-/* Ctrl-C tells the terminal to send a SIGINT to the current foreground process, 
-*   which by default translates into terminating the application. 
-*   Ctrl-D tells the terminal that it should register a EOF on standard input, 
-*   which bash interprets as a desire to exit.
-*/
