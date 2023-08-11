d6cbd857dcd01730e6e05f13b752e4aa04fb1938
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-08-11 16:17:57 +0200

    Update signals.c

--- src/utils/signals.c
+++ src/utils/signals.c
@@ -6,7 +6,7 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/06 10:48:39 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/08/10 13:34:06 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/08/11 16:17:03 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
  * Since 'signal()' can't directly pass a struct, 
  * using a global variable is a workaround to capture the exit status.
  */
-int	g_exit_status = 0;
+int	g_exit_status;
 
 /* restore_prompt();
  *	- Signal handler for SIGINT (Ctrl-C) in the parent process.
@@ -29,17 +29,11 @@ void	restore_prompt(int sig)
 {
 	if (sig == SIGINT)
 	{
-		// struct termios term;
-        // tcgetattr(STDIN_FILENO, &term);
-        // term.c_lflag |= ECHOCTL; // Turn on ECHOCTL flag to hide ^C
-        // tcsetattr(STDIN_FILENO, TCSANOW, &term);
-        
+		rl_replace_line("", 0);
 		ft_putchar_fd('\n', STDOUT_FILENO);
 		rl_on_new_line();
-		rl_replace_line("", 0);
 		rl_redisplay();
 		g_exit_status = 128 + sig;
-		
 	}
 }
 
@@ -51,20 +45,20 @@ void	ctrl_c(int sig)
 {
 	if (sig == SIGINT)
 	{
-		ft_putchar_fd('\n', STDOUT_FILENO);
+		ft_putstr_fd("^C\n", STDOUT_FILENO);
 		g_exit_status = 128 + sig;
 	}
 }
 
 /* backslash();
  *	- Signal handler for SIGQUIT (Ctrl-\).
- *	- Writes "Quit" with the sig to the output and updates the exit status.
+ *	- Writes "^\Quit" with the sig to the output and updates the exit status.
  */
 void	backslash(int sig)
 {
 	if (sig == SIGQUIT)
 	{
-		ft_putstr_fd("Quit: ", STDERR_FILENO);
+		ft_putstr_fd("^\\Quit: ", STDERR_FILENO);
 		ft_putnbr_fd(sig, STDERR_FILENO);
 		ft_putchar_fd('\n', STDERR_FILENO);
 		g_exit_status = 128 + sig;
@@ -78,6 +72,14 @@ void	backslash(int sig)
  *		- t_data *data: main struct, for the exit status;
  *
  *	- Configures the signal handling for the minishell.
+ *	- struct termios: is used to store terminal attributes, 
+ *	  which control various aspects of terminal behavior.
+ *	- tcgetattr: retrieves the terminal attributes.
+ *	- The ECHOCTL flag is turns off (clears), 
+ *	  so the control characters won't be echoed.
+ *		- c_lflag: local flags.
+ *		- ECHOCTL: controls whether the terminal echoes control 
+ *	  	  characters as ^X. 
  *	- When 'num' is 1 (signal is in the parent process):
  *		- SIGINT is set to call the 'restore_prompt' function 
  *		  when received (used for Ctrl-C).
@@ -94,9 +96,14 @@ void	backslash(int sig)
  */
 void	handle_signal(int num, t_data *data)
 {
+	struct termios	term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &term);
 	if (num == 1)
 	{
-		signal(SIGINT, restore_prompt); //Turn of ^C
+		signal(SIGINT, restore_prompt);
 		signal(SIGQUIT, SIG_IGN);
 	}
 	if (num == 2)
